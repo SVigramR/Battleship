ea73afd512f50987b0141bf57ccd4197c0c4b498
SVigramR <sabareeshvigram654@gmail.com>
2024-02-02 06:26:16 +0530

    Added HTML and bundling to the project

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: "development",
   entry: './src/main.js',
   output: {
    filename: 'bundle.js',
@@ -8,10 +10,25 @@ module.exports = {
   },
   module: {
     rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
-};
+        {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+        },
+        {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+        },
+        {
+            test: /\.(woff|woff2|eot|ttf|otf)$/i,
+            type: 'asset/resource',
+        },
+    ]
+},
+plugins: [
+    new HtmlWebpackPlugin({
+        title: 'Battleship',
+        filename: 'index.html',
+        template: 'src/index.html',
+    }),
+]
+}
