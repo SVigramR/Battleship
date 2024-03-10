121bcd5c3efa06908b8b5fcf70c46f89a23e6696
SVigramR <sabareeshvigram654@gmail.com>
2024-03-10 18:07:06 +0530

    Fixing the placeBoard append bug

--- src/modules/popup.js
+++ src/modules/popup.js
@@ -4,6 +4,13 @@ function popupListener() {
     const openTaskForm = document.querySelector('#startBtn')
     const popupBackground = document.querySelectorAll('[data-background]')
     const popupClose = document.querySelectorAll('[data-close]')
+    const placeBoard = document.querySelector('#placeBoard')
+
+    const removeDivs = () => {
+        while (placeBoard.firstChild) {
+            placeBoard.removeChild(placeBoard.firstChild)
+        }
+    }
 
     openTaskForm.addEventListener('click', () => {
         document.getElementById('addTaskPopup').classList.add('active')
@@ -13,12 +20,14 @@ function popupListener() {
     popupBackground.forEach(background => {
         background.addEventListener('click', () => {
             document.getElementById('addTaskPopup').classList.remove('active')
+            removeDivs()
         })
     })
 
     popupClose.forEach(close => {
         close.addEventListener('click', () => {
             document.getElementById('addTaskPopup').classList.remove('active')
+            removeDivs()
         })
     })
 }
