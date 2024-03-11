8642d6fbe50a3e4e9a20be479eee6e8956e491dd
SVigramR <sabareeshvigram654@gmail.com>
2024-03-11 14:48:29 +0530

    Added UI for placing ships through input

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,6 +1,5 @@
 const playerBoard = document.getElementById('playerBoard')
 const computerBoard = document.getElementById('computerBoard')
-const placeBoard = document.getElementById('placeBoard')
 const classArray = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59', '60', '61', '62', '63', '64', '65', '66', '67', '68', '69', '70', '71', '72', '73', '74', '75', '76', '77', '78', '79', '80', '81', '82', '83', '84', '85', '86', '87', '88', '89', '90', '91', '92', '93', '94', '95', '96', '97', '98', '99']
 
 function gameBoardModal() {
@@ -12,8 +11,9 @@ function gameBoardModal() {
         playerBoard.appendChild(div)
         computerBoard.appendChild(compDiv)
     }
-}
+}   
 
+const placeBoard = document.getElementById('placeBoard')
 function placeShipModal() {
     for (let index = 0; index < 100; index++) {
         let div = document.createElement('div')
@@ -21,12 +21,6 @@ function placeShipModal() {
         placeBoard.appendChild(div)
     }
 }
-
-function shipModal() {}
-
-function dragAndPlace() {}
-
-function resultModal() {}
 
 export {
     gameBoardModal,
